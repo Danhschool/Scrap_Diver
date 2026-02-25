53b447816030d24d936e07984ff8eed7fe9caa64
Danh School <danhtruongk3@gmail.com>
2026-02-25 14:36:45 +0700

    sua he thong thu thach

---
+++ Assets/Scripts/Achievement/AchievementData.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct AchievementStage
+{
+    public float targetValue;
+    public int rewardCoins;
+}
+
+[CreateAssetMenu(fileName = "Achievement", menuName = "Systems/Achievement Data")]
+public class AchievementData : ScriptableObject
+{
+    [Header("Info")]
+    public string id;
+    public string title;
+    [TextArea] public string descriptionTemplate;
+    public Sprite icon;
+
+    [Header("Logic")]
+    // Tham chiếu đến Strategy
+    public AchievementCondition condition;
+
+    [Header("Robot")]
+    public bool isRobotSpecific;
+    public string robotID;
+
+    [Header("Levels")]
+    public List<AchievementStage> stages;
+}
+
+//using UnityEngine;
+//using System.Collections.Generic;
+
+//public enum AchievementType
+//{
+//    SingleRunCoins,
+//    SingleRunTime,
+//    TotalRobotsOwned,
+//    ReachLevelIndex
+//}
+
+//[System.Serializable]
+//public struct AchievementStage
+//{
+//    public float targetValue;
+//    public int rewardCoins;
+//}
+
+//[CreateAssetMenu(fileName = "NewAchievement", menuName = "Systems/Achievement Data")]
+//public class AchievementData : ScriptableObject
+//{
+//    [Header("Info")]
+//    public string id;
+//    public string title;
+//    [TextArea] public string descriptionTemplate;
+//    public Sprite icon;
+
+//    [Header("Logic")]
+//    public AchievementType type;
+
+//    [Header("Robot")]
+//    public bool isRobotSpecific;
+//    public string robotID;
+
+//    [Header("Levels")]
+//    public List<AchievementStage> stages;
+//}
---
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager instance;
+    [SerializeField] private List<AchievementData> allAchievements;
+
+    public List<AchievementData> AllAchievements => allAchievements;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CheckEndRunAchievements(RunStats stats)
+    {
+        foreach (var ach in allAchievements)
+        {
+            int currentLv = DataManager.GetAchievementLevel(ach.id);
+
+            if (currentLv >= ach.stages.Count) continue;
+            if (ach.isRobotSpecific && ach.robotID != stats.robotID) continue;
+
+            AchievementStage currentStage = ach.stages[currentLv];
+
+            // Nếu chưa gán condition trong Inspector, bỏ qua để tránh lỗi NullReference
+            if (ach.condition == null) continue;
+
+            // Ủy quyền xử lý logic cho đối tượng condition
+            bool isCompleted = ach.condition.CheckCompletion(stats, currentStage.targetValue);
+
+            if (isCompleted)
+            {
+                UnlockLevel(ach, currentLv);
+            }
+        }
+    }
+
+    private void UnlockLevel(AchievementData ach, int currentLv)
+    {
+        DataManager.SetAchievementLevel(ach.id, currentLv + 1);
+    }
+
+    public int GetCurrentLevel(string id)
+    {
+        return DataManager.GetAchievementLevel(id);
+    }
+
+    #region old code
+    //public static AchievementManager instance;
+    //[SerializeField] private List<AchievementData> allAchievements;
+
+    //public List<AchievementData> AllAchievements => allAchievements;
+
+    //private void Awake()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //    else
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    //public void CheckEndRunAchievements(RunStats stats)
+    //{
+    //    foreach (var ach in allAchievements)
+    //    {
+    //        int currentLv = DataManager.GetAchievementLevel(ach.id);
+
+    //        if (currentLv >= ach.stages.Count) continue;
+    //        if (ach.isRobotSpecific && ach.robotID != stats.robotID) continue;
+
+    //        AchievementStage currentStage = ach.stages[currentLv];
+    //        bool isCompleted = false;
+
+    //        switch (ach.type)
+    //        {
+    //            case AchievementType.SingleRunCoins:
+    //                if (stats.coinsCollected >= currentStage.targetValue) isCompleted = true;
+    //                break;
+    //            case AchievementType.SingleRunTime:
+    //                if (stats.timeAlive >= currentStage.targetValue) isCompleted = true;
+    //                break;
+    //        }
+
+    //        if (isCompleted)
+    //        {
+    //            UnlockLevel(ach, currentLv);
+    //        }
+    //    }
+    //}
+
+    //private void UnlockLevel(AchievementData ach, int currentLv)
+    //{
+    //    DataManager.SetAchievementLevel(ach.id, currentLv + 1);
+    //}
+
+    //public int GetCurrentLevel(string id)
+    //{
+    //    return DataManager.GetAchievementLevel(id);
+    //}
+    #endregion
+}
